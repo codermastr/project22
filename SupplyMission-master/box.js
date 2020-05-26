d1d6ceab9f8718be1285610d5051bbff7a07b6d0
codermastr <65798699+codermastr@users.noreply.github.com>
2020-05-26 18:17:32 +0800

    Add files via upload

---
+++ SupplyMission-master/box.js
@@ -0,0 +1,9 @@
+ class Box{
+     constructor(x,y,width,height,angle){
+         var options ={
+             'restitution':0.8,
+             'friction':5.0,
+             'density':1.0
+         }
+     }
+ }
